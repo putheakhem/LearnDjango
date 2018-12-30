2ef725aad0d8bde1f495637afc6a6930de92a251
putheakhem <puthea.khem@gmail.com>
2018-12-30 23:21:53 +0700

    register admin model

--- courses/models.py
+++ courses/models.py
@@ -10,3 +10,15 @@ class Course(models.Model):
     
     def __str__(self):
         return self.title
+
+class Step(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(
+        Course,
+        on_delete = models.DO_NOTHING
+    )
+
+    def __str__(self):
+        return self.title
